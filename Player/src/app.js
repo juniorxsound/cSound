58e97b6d9181a2a168b276349c619c6d4b6d6885
Or Fleisher <contact@orfleisher.com>
2016-10-05 22:15:23 -0400

    init the player with the cSound PNaCl
    Getting the foundations of the player down.
    1. added panel lib with sound.mnf & unstripped binary version of cSound

---
+++ Player/src/app.js
@@ -0,0 +1,58 @@
+function moduleDidLoad() {
+
+}
+function attachListeners() {
+ document.getElementById('playButton').
+      addEventListener('click', togglePlay);
+  document.getElementById('files').
+     addEventListener('change', handleFileSelect, false);
+}
+
+var count = 0;
+function handleMessage(message) {
+       var element = document.getElementById('console');
+       element.value += message.data;
+       element.scrollTop = 99999; // focus on bottom
+       count += 1;
+       if(count == 1000) {
+          element.value = ' ';
+          count = 0;
+       }
+ }
+
+
+var playing = false;
+var started = false;
+var loaded = false;
+var fname;
+
+function togglePlay(){
+ if(loaded) {
+ if(!playing) {
+ if(started) csound.Play();
+ else {
+  csound.PlayCsd("local/" + fname)
+  started = true;
+ }
+ document.getElementById('playButton').innerText = "Pause";
+ playing = true;
+ } else {
+ csound.Pause()
+ document.getElementById('playButton').innerText ="Play";
+ playing = false;
+ }
+}
+}
+
+function handleFileSelect(evt) {
+  if(!loaded) {
+   var files = evt.target.files;
+   var f = files[0];
+   var objectURL = window.URL.createObjectURL(f);
+   csound.CopyUrlToLocal(objectURL, f.name);
+   fname = f.name;
+   loaded = true;
+  } else {
+  csound.updateStatus("to load a new CSD, first refresh page!")
+ }
+}
